c62f5a2d42fe2bfcaf5c23b6135d692f4e1d058c
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2022-08-27 12:10:03 +0200

    Add Azure DevOps CI support

---
+++ .ci/package.ps1
@@ -0,0 +1,6 @@
+mkdir ${env:buildPath}\.dist | Out-Null
+
+Set-Location "${env:buildPath}\bin\${env:_RELEASE_CONFIGURATION}"
+7z a "${env:buildPath}\.dist\${env:_RELEASE_NAME}-${env:_RELEASE_VERSION}_${env:_RELEASE_CONFIGURATION}.zip" ".\*"
+
+Copy-Item *.nupkg ${env:buildPath}\.dist
